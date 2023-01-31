518df76439132c5465fc5964e7fec9520f7e75a1
Radina Yaneva <radina_yaneva@yahoo.com>
2023-01-31 22:57:35 +0200

    Add problems Solutions

---
+++ StreamsFilesAndDirectoriesLab/P01ReadFile.java
@@ -0,0 +1,38 @@
+package StreamsFilesAndDirectoriesLab;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Scanner;
+
+public class P01ReadFile {
+
+    public static void main(String[] args) throws IOException {
+
+
+        String filePath = "/Users/macbookpro/Desktop/Java/JavaAdvancedJan2023/04. Java-Advanced-Files-and-Streams-Lab-Resources/input.txt";
+
+        try(InputStream inputStream = new FileInputStream(filePath)) {
+
+            int readByte = inputStream.read();
+            while (readByte >= 0) {
+
+                System.out.print(Integer.toBinaryString(readByte) + " ");
+                readByte = inputStream.read();
+            }
+        } catch (IOException e) {
+
+            throw  new RuntimeException(e);
+        }
+
+//        InputStream inputStream = new FileInputStream(filePath);
+//
+//        int firstByte = inputStream.read();
+//
+//        System.out.println(firstByte);
+//        System.out.println((char)firstByte);
+//
+//        inputStream.close();
+    }
+}
