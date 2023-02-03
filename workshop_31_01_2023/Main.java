19cdd8c073dfd0a2668017ddea3e86f48ed026f2
Radina Yaneva <radina_yaneva@yahoo.com>
2023-02-03 14:49:52 +0200

    Workshop added

---
+++ workshop_31_01_2023/Main.java
@@ -0,0 +1,21 @@
+package workshop_31_01_2023;
+
+public class Main {
+    public static void main(String[] args) {
+//        int[] data = new int[4];
+//        SmartArray smartArray = new SmartArray(data);
+//
+//        smartArray.add(1);
+//        smartArray.add(2);
+//        smartArray.add(3);
+//        smartArray.add(4);
+//
+//        smartArray.forEach(el -> System.out.println(el));
+
+        CustomStack customStack = new CustomStack();
+
+
+        customStack.pop();
+
+    }
+}
